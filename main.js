9a9811d4e0530803b6ff866d701efcf22bd0e833
Zatruc <yosanva123@gmail.com>
2024-12-26 14:43:00 -0600

    I think it works

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const $selectLanguaje = document.getElementById("select_languaje");
 const $state = document.querySelector(".state");
-const $refresh = document.querySelector(".refresh");
-const $retry = document.querySelector(".retry");
-const $extraState = document.querySelector(".extra");
+const $refresh = document.getElementById("refresh");
+const $retry = document.getElementById("retry");
 
 // load languajes to the select
 document.addEventListener("DOMContentLoaded", async () => {
@@ -26,16 +25,35 @@ document.addEventListener("DOMContentLoaded", async () => {
 const handleState = (state) => {
   switch (state) {
     case "loading":
-      $state.innerHTML = "Loading, please wait...";
+      $refresh.style.display = "none";
+      $retry.style.display = "none";
+      $state.textContent = "Loading, please wait...";
+      $state.style.justifyContent = "center";
+      $state.style.backgroundColor = "#E9ECEF";
+      $state.style.border = "none";
       break;
     case "empty":
-      $state.innerHTML = "Please select a languaje";
+      $retry.style.display = "none";
+      $refresh.style.display = "none";
+      $state.textContent = "Please select a languaje";
+      $state.style.justifyContent = "center";
+      $state.style.backgroundColor = "#E9ECEF";
+      $state.style.border = "none";
       break;
     case "error":
+      $refresh.style.display = "none";
+      $retry.style.display = "block";
+      $retry.addEventListener("click", handleSelectLanguaje);
       $state.innerHTML = "Error fetching repositories";
+      $state.style.backgroundColor = "#FFC9C9";
+      $state.style.border = "none";
       break;
     case "success":
       $state.innerHTML = "";
+      $refresh.style.display = "block";
+      $retry.style.display = "none";
+      $refresh.addEventListener("click", handleSelectLanguaje);
+      $state.style.border = "solid";
       break;
     default:
       $state.innerHTML = "";
@@ -61,21 +79,21 @@ const handleSelectLanguaje = async () => {
     console.log(randomRepo);
     const name = document.createElement("h2");
     const description = document.createElement("p");
-    const stars = document.createElement("p");
-    const forks = document.createElement("p");
-    const openIssues = document.createElement("p");
 
     name.textContent = randomRepo.name;
-    description.textContent = randomRepo.description;
-    stars.textContent = randomRepo.stargazers_count;
-    forks.textContent = randomRepo.forks;
-    openIssues.textContent = randomRepo.open_issues;
+    !randomRepo.description
+      ? (description.textContent = "No description")
+      : (description.textContent = randomRepo.description);
 
     $state.appendChild(name);
     $state.appendChild(description);
-    $extraState.appendChild(stars);
-    $extraState.appendChild(forks);
-    $extraState.appendChild(openIssues);
+
+    const extra = document.createElement("div");
+    extra.classList.add("extra");
+
+    createItems(randomRepo).forEach((item) => extra.appendChild(item));
+
+    $state.appendChild(extra);
   } catch (error) {
     console.error(error);
     handleState("error");
@@ -83,3 +101,34 @@ const handleSelectLanguaje = async () => {
 };
 
 $selectLanguaje.addEventListener("change", handleSelectLanguaje);
+
+const createItems = (randomRepo) => {
+  const starContainer = document.createElement("div");
+  const starIcon = document.createElement("img");
+  starIcon.src = "./icons/stars.svg";
+  starContainer.classList.add("item");
+
+  const issueContainer = document.createElement("div");
+  const issueIcon = document.createElement("img");
+  issueIcon.src = "./icons/issues.svg";
+  issueContainer.classList.add("item");
+
+  const forkContainer = document.createElement("div");
+  const forkIcon = document.createElement("img");
+  forkIcon.src = "./icons/forks.svg";
+  forkContainer.classList.add("item");
+
+  const stars = document.createElement("span");
+  const forks = document.createElement("span");
+  const openIssues = document.createElement("span");
+
+  stars.textContent = randomRepo.stargazers_count;
+  forks.textContent = randomRepo.forks;
+  openIssues.textContent = randomRepo.open_issues;
+
+  starContainer.append(starIcon, stars);
+  issueContainer.append(issueIcon, openIssues);
+  forkContainer.append(forkIcon, forks);
+
+  return [starContainer, issueContainer, forkContainer];
+};
